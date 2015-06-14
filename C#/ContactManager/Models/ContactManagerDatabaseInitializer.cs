c13ee14f48fd1f71072ae6fd4d01f29c5e0ac070
Quang Ly <quangdly@gmail.com>
2015-06-14 10:31:05 -0400

    WebAPI Contact Manager / Mobile

---
+++ C#/ContactManager/Models/ContactManagerDatabaseInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace ContactManager.Models
+{   
+    //public class ContactManagerDatabaseInitializer : DropCreateDatabaseAlways <ContactManagerContext>
+    public class ContactManagerDatabaseInitializer : DropCreateDatabaseIfModelChanges <ContactManagerContext>
+    {
+        protected override void Seed(ContactManagerContext context)
+        {
+            base.Seed(context);
+
+            foreach (var contact in SampleData.Contacts)
+            {
+                context.Contacts.Add(contact);
+            }
+
+        }
+    }
+}
